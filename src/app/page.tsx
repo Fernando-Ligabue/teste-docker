b1773c5a7dbd5e3edaa9e022599a74303a6e36d4
Fernando Ligabue <nandoligabue@gmail.com>
2025-10-15 17:57:17 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import { getStrapiDataAbout, getStrapiDataHome } from "@/lib/actions";
+
+export default async function Home() {
+  const strapiDataHomeContent = await getStrapiDataHome("api/home-page");
+  console.log(strapiDataHomeContent);
+  const strapiDataAboutContent = await getStrapiDataAbout("api/about");
+
+  const { title, description } = strapiDataHomeContent.data;
+  const { titleAbout, descriptionAbout } = strapiDataAboutContent.data;
+
+  // const imageUrl = homebg?.url ? `${baseUrl}${homebg.url}` : null;
+
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16">
+      <main className="flex flex-col gap-32">
+        <section
+          className="w-full min-h-[75vh] flex flex-col items-center justify-center gap-8 bg-cyan-200"
+          // style={{
+          //   backgroundImage: imageUrl ? `url('${imageUrl}')` : 'none',
+          //   backgroundSize: 'cover',
+          //   backgroundPosition: 'center',
+          // }}
+        >
+          <h1 className="text-5xl text-black">{title}</h1>
+          <p className="text-5xl text-white">{description}</p>
+        </section>
+
+        <section className="w-full h-screen flex flex-col items-center justify-center gap-8">
+          <h1 className="text-5xl text-black">{titleAbout}</h1>
+          <p className="text-5xl text-slate-600">{descriptionAbout}</p>
+        </section>
+      </main>
+    </div>
+  );
+}
