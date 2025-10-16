535ce8a03c1ae5aca4feefe188268f3bc29441c6
Fernando Ligabue <nandoligabue@gmail.com>
2025-10-16 16:55:34 +0100

    Alteraçãpo de deploy.yml com conexão ao plesk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default async function Home() {
   const { title, description } = strapiDataHomeContent.data;
   const { titleAbout, descriptionAbout } = strapiDataAboutContent.data;
 
-  // const imageUrl = homebg?.url ? `${baseUrl}${homebg.url}` : null;
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16">
@@ -18,10 +17,10 @@ export default async function Home() {
           <h1 className="text-5xl text-black">{title}</h1>
           <p className="text-5xl text-white">{description}</p>
                     <p className="text-2xl text-black">Alteração accesstoken</p>
-
         </section>
 
         <section className="w-full h-screen flex flex-col items-center justify-center gap-8">
+          <small>Teste deploy com pull no plesk</small>
           <h1 className="text-5xl text-black">{titleAbout}</h1>
           <p className="text-5xl text-slate-600">{descriptionAbout}</p>
         </section>
